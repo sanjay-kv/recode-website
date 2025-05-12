4bc5f517c132abfd33d79ab2bb95628fc5dfbdf1
Vansh Chaurasiya <vanshchaurasiya1557@gmail.com>
2025-05-12 23:17:11 +0530

    Feat:#14 Make images in blog clickable

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -11,7 +11,7 @@ export default function Blogs(): React.JSX.Element {
   return (
     <Layout
       title={`Blogs from ${siteConfig.title}`}
-      description="Welcome to CodeHarborHub Blogs. Learn the basics to advanced concepts of web development including HTML, CSS, JavaScript, React, Node.js, DSA, and more."
+      description="Welcome to RecodeHive Blogs. Learn the basics to advanced concepts of web development including HTML, CSS, JavaScript, React, Node.js, DSA, and more."
     >
       <Head>
         <script
@@ -29,7 +29,7 @@ export default function Blogs(): React.JSX.Element {
       <section className="m-4 my-10">
         <div className="text-center mb-8">
           <h1 className="text-4xl font-bold gradient-text">
-            Welcome to CodeHarborHub Blogs
+            Welcome to RecodeHive Blogs
           </h1>
           <p className="text-lg text-gray-700 dark:text-gray-300">
             Discover web development articles ranging from HTML and CSS to
@@ -50,13 +50,15 @@ export default function Blogs(): React.JSX.Element {
 const BlogCard = ({ blog }) => {
   return (
     <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden">
-      <img
-        src={blog.image}
-        alt={blog.title}
-        className="w-full h-48 object-cover"
-      />
+      <Link to={`/blog/${blog.slug}`}>
+        <img
+          src={blog.image}
+          alt={blog.title}
+          className="w-full h-48 object-cover"
+        />
+      </Link>
       <div className="p-4">
-        <h2 className="text-xl font-semibold dark:text-white">{blog.title}</h2>
+        <h2 className="text-xl font-semibold text-white">{blog.title}</h2>
         <p className="text-gray-600 dark:text-gray-400 mb-4">
           {blog.description}
         </p>
