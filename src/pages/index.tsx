dc2c389c8d1c5a7d43b86ee400aa89b5892edff2
iitzIrFan <irfanlhawk@gmail.com>
2025-06-08 18:00:16 +0530

    refactor: remove AdsComponent and update related scripts in blog, home, sponsors, and showcase pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,15 +47,9 @@ export default function Home(): ReactNode {
       <Head>
         <script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5832817025080991"
-          crossOrigin="anonymous"
-        />
-        <script
-          async
           custom-element="amp-auto-ads"
           src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"
         />
-        <meta name="google-adsense-account" content="ca-pub-5832817025080991" />
       </Head>
       <main className="bg-gradient-to-b from-white to-white/50">
         <div className="m-4">
--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,18 +1,14 @@
-import AdsComponent from "@site/src/components/AdsComponent";
 import BrowserWindow from "@site/src/components/BrowserWindow";
 import MDXComponents from "@theme-original/MDXComponents";
-
 import TabItem from "@theme/TabItem";
 import Tabs from "@theme/Tabs";
 import Comming from "../components/Comming";
 import Image from "@theme/IdealImage";
 import GiscusComments from "../components/giscus";
 
-
 export default {
   ...MDXComponents,
   BrowserWindow,
-  AdsComponent,
   Tabs,
   TabItem,
   Comming,
